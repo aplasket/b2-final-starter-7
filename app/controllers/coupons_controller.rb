c9df78344d76abcc4a6b1d67c49c474020c8b396
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-09 17:31:22 -0400

    update controller with new and create actions

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,11 +1,24 @@
 class CouponsController < ApplicationController
-  before_action :find_merchant, only: [:index]
+  before_action :find_merchant, only: [:index, :new, :create]
 
   def index
     @coupons = @merchant.coupons
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+    Coupon.create!(name: params[:name],
+                  unique_code: params[:unique_code],
+                  amount_off: params[:amount_off],
+                  discount_type: params[:discount_type],
+                  merchant: @merchant)
+    flash.notice = "New Coupon has been created!"
+    redirect_to merchant_coupons_path(@merchant)
   end
 
   private
