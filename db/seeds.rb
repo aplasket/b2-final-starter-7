161bd3d7dfbc15a77ec457a55e36b2a5c12c8509
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-09 15:01:34 -0400

    add some coupon examples

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,11 @@ Customer.destroy_all
 Merchant.destroy_all
 
 Rake::Task["csv_load:all"].invoke
+
+@hair = Merchant.create!(name: "Hair Care")
+@hair10 = Coupon.create!(name: "10% off", unique_code: "HAIR10OFF", amount_off: 10, discount_type: 0, merchant_id: @hair.id)
+@hair20 = Coupon.create!(name: "20% off", unique_code: "HAIR20OFF", amount_off: 20, discount_type: 0, merchant_id: @hair.id)
+@hairbogo50 = Coupon.create!(name: "Free Shipping", unique_code: "HAIRFREESHIP", amount_off: 7, discount_type: 1, merchant_id: @hair.id)
+
+@sallys = Merchant.create!(name: "Sally's Salon")
+@sallybogo50 = Coupon.create!(name: "Free Shipping", unique_code: "SALLYFREESHIP", amount_off: 12, discount_type: 1, merchant_id: @sallys.id)
