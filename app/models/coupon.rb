8f4245b62f2c8ff172a2f2961a67b7d101c63e40
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-09 17:43:55 -0400

    update validations for numericality of amount_off

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,14 +1,15 @@
 class Coupon < ApplicationRecord
   validates_presence_of :name,
                         :unique_code,
-                        :amount_off,
                         :discount_type,
                         :status,
                         :merchant_id
 
+  validates :amount_off, numericality: { only_integer: true, greater_than: 0 }
+
   belongs_to :merchant
   has_many :invoices
-  
+
   enum discount_type: [:percent, :dollars]
   enum status: [:active, :inactive]
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Coupon, type: :model do
   it { should belong_to(:merchant) }
   it { should have_many(:invoices) }
-  
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:unique_code) }
-  it { should validate_presence_of(:amount_off) }
+  it { should validate_numericality_of(:amount_off) }
   it { should validate_presence_of(:discount_type) }
   it { should validate_presence_of(:status) }
   it { should validate_presence_of(:merchant_id) }
