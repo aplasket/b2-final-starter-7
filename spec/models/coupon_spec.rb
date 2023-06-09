b1d1d31bc1721c45420b8bf7b84ddae1061a1047
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-09 12:57:38 -0400

    add spec validations

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Coupon, type: :model do
   it { should belong_to(:merchant) }
   it { should belong_to(:invoice).optional }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:unique_code) }
+  it { should validate_presence_of(:amount_off) }
+  it { should validate_presence_of(:discount_type) }
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:merchant_id) }
 end
